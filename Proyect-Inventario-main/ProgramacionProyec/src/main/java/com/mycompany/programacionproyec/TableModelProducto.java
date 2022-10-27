13ce97925fc299e73d0be591355db06e25516e80
Dicmar881 <85083981+Dicmar881@users.noreply.github.com>
2022-10-27 01:33:17 -0600

    Agregrando clases
    Corrigiendo errores

---
+++ Proyect-Inventario-main/ProgramacionProyec/src/main/java/com/mycompany/programacionproyec/TableModelProducto.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.programacionproyec;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author HP
+ */
+public class TableModelProducto extends AbstractTableModel{
+
+    private String[] columnas = {"Codigo", "Nombre", "Precio", "Inventario"};
+    private List<Producto> producto = new ArrayList<>();
+
+    public TableModelProducto(List<Producto> prod) {
+        this.producto = prod;
+    }
+    
+ 
+    @Override
+    public int getRowCount() {
+        return this.producto.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return this.columnas.length; 
+    }
+
+    @Override
+    public Object getValueAt(int fila, int columna) {
+        Object resp;
+        
+        switch(columna){
+            case 0:
+                resp = this.producto.get(fila).getCodigo();
+                break;
+            case 1:
+                resp = this.producto.get(fila).getNombre();
+                break;
+            case 2:
+                resp = this.producto.get(fila).getPrecio ();
+                break;
+            default:
+                 resp = this.producto.get(fila).getInventario();
+        }
+        return resp;
+    }
+    @Override
+    public String getColumnName(int colum){
+       return this.columnas[colum];
+    }
+    
+    public void actualizarTabla(){
+        fireTableDataChanged();
+    }
+    
+    public Producto detalle(int fila){
+        return this.producto.get(fila);     
+    }
+    
+   
+}
